bab72a171e78730730647896f58c4efe53f821e8
Jan Vcelak <jvcelak@redhat.com>
2011-09-27 23:46:08 +0200

    throughput-performance: add cpu_dma_latency=0

--- tune-profiles/throughput-performance/ktune.sh
+++ tune-profiles/throughput-performance/ktune.sh
@@ -5,6 +5,7 @@
 start() {
 	set_cpu_governor performance
 	enable_transparent_hugepages
+	/usr/libexec/tuned/pmqos-static.py cpu_dma_latency=0
 
 	return 0
 }
@@ -12,6 +13,7 @@ start() {
 stop() {
 	restore_cpu_governor
 	restore_transparent_hugepages
+	/usr/libexec/tuned/pmqos-static.py disable
 
 	return 0
 }
