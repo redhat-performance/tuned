31639fdec76b294fd67c78ec332fe26bf0ad7bb9
Jan Vcelak <jvcelak@redhat.com>
2011-09-27 23:55:21 +0200

    latency-performance: add cpu_dma_latency=0

--- tune-profiles/latency-performance/ktune.sh
+++ tune-profiles/latency-performance/ktune.sh
@@ -4,12 +4,14 @@
 
 start() {
 	set_cpu_governor performance
+	/usr/libexec/tuned/pmqos-static.py cpu_dma_latency=0
 
 	return 0
 }
 
 stop() {
 	restore_cpu_governor
+	/usr/libexec/tuned/pmqos-static.py disable
 
 	return 0
 }
