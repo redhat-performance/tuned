8bb2e57c523cc27915b422d077ba5c644bd85f6a
Jan Vcelak <jvcelak@redhat.com>
2012-01-11 12:48:19 +0100

    add virtual-guest profile

---
+++ tune-profiles/virtual-guest/ktune.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+. /etc/tune-profiles/functions
+
+start() {
+	set_cpu_governor performance
+#	set_disk_scheduler_quantum 10
+	enable_transparent_hugepages
+
+	# Find non-root and non-boot partitions, disable barriers on them
+	rootvol=$(df -h / | grep "^/dev" | awk '{print $1}')
+	bootvol=$(df -h /boot | grep "^/dev" | awk '{print $1}')
+	volumes=$(df -hl --exclude=tmpfs | grep "^/dev" | awk '{print $1}')
+
+	nobarriervols=$(echo "$volumes" | grep -v $rootvol | grep -v $bootvol)
+	remount_partitions nobarrier $nobarriervols
+
+	multiply_disk_readahead 4
+
+	return 0
+}
+
+stop() {
+	restore_cpu_governor
+#	restore_disk_scheduler_quantum
+	restore_transparent_hugepages
+
+	# Find non-root and non-boot partitions, re-enable barriers
+	rootvol=$(df -h / | grep "^/dev" | awk '{print $1}')
+	bootvol=$(df -h /boot | grep "^/dev" | awk '{print $1}')
+	volumes=$(df -hl --exclude=tmpfs | grep "^/dev" | awk '{print $1}')
+
+	nobarriervols=$(echo "$volumes" | grep -v $rootvol | grep -v $bootvol)
+	remount_partitions barrier $nobarriervols
+
+	multiply_disk_readahead 0.25
+
+	return 0
+}
+
+process $@
