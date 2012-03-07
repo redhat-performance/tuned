c4a0fd2cf160ecaf3152397ac62996635322a131
Jan Kaluza <hanzz.k@gmail.com>
2012-03-07 13:07:17 +0100

    store in /var/lib

--- tuned/utils/storage.py
+++ tuned/utils/storage.py
@@ -23,7 +23,7 @@ import os
 
 log = tuned.logs.get()
 
-DEFAULT_STORAGE_FILE = "./save.pickle"
+DEFAULT_STORAGE_FILE = "/var/lib/tuned/save.pickle"
 
 class Storage(tuned.patterns.Singleton):
 	def __init__(self):
