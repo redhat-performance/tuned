0faaa74df301a9772076964db89f182b4a207df6
Jan Kaluza <hanzz.k@gmail.com>
2012-03-13 13:20:52 +0100

    tuned.cfg man page + initial tuned-adm

---
+++ tuned-adm.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+#
+# tuned-adm: A command line utility for switching between user 
+#            definable tuning profiles.
+#
+# Copyright (C) 2012 Red Hat, Inc.
+# Authors: Jan Kaluza
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+import os
+import sys
+import locale
+import signal
+
+PIDFILE = "/var/run/tuned/tuned.pid"
+
+def usage():
+	print """
+Usage: tuned-adm <command>
+
+commands:
+  help                           show this help message and exit
+  list                           list all available and active profiles
+  active                         show current active profile
+  off                            switch off all tunning
+  profile <profile-name>         switch to given profile
+"""
+
+class Tuned_adm:
+
+	def error(self, msg, exit_code = 1):
+		print >>sys.stderr, msg
+		sys.exit(exit_code)
+
+	def check_permissions(self):
+		if not os.getuid() == 0:
+			self.error("Only root can run this script.", 2)
+
+	def run(self, args):
+		if args[0] == "list":
+			#self.list()
+			pass
+		elif args[0] == "active":
+			#self.active()
+			#self.service_status("tuned")
+			#self.service_status("ktune")
+			pass
+		elif args[0] == "off":
+			self.check_permissions()
+			#self.off()
+		elif args[0] == "profile":
+			if len(args) == 2:
+				self.check_permissions()
+				self.set_active_profile(args[1])
+			else:
+				self.error("Invalid profile specification. Use 'tuned-adm list' to get all available profiles.")
+		else:
+			self.error("Nonexistent argument '%s'." % args[0])
+
+	def set_active_profile(self, profile):
+		pid = 0
+		try:
+			with open(PIDFILE) as f:
+				pid = int(f.read())
+		except (OSError,IOError) as e:
+			self.error("Cannot read %s: %s" % (PIDFILE, str(e)))
+
+		if pid:
+			try:
+				with open("/etc/tuned/active_profile", "w") as f:
+					f.write(profile)
+			except (OSError,IOError) as e:
+				log.error("Cannot write profile into /etc/tuned/active_profile: %s" % (e))
+			os.kill(pid, signal.SIGHUP)
+		
+
+if __name__ == "__main__":
+	args = sys.argv[1:]
+
+	if len(args) < 1:
+		print >>sys.stderr, "Missing arguments."
+		usage()
+		sys.exit(1)
+
+	if args[0] in [ "help", "--help", "-h" ]:
+		usage()
+		sys.exit(0)
+
+	tuned_adm = Tuned_adm()
+	tuned_adm.run(args)
+
