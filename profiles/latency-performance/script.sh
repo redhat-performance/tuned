84f338461f948415b27839579036b62a4748aa33
Jan Vcelak <jvcelak@redhat.com>
2012-04-24 17:36:26 +0200

    refs #13, latency-performance: use cpu force_latency

--- profiles/latency-performance/script.sh
+++ profiles/latency-performance/script.sh
@@ -5,7 +5,6 @@
 start() {
     set_elevator "/sys/block/{sd,cciss,dm-,vd}*/queue/scheduler" deadline
     set_cpu_governor performance
-    /usr/libexec/tuned/pmqos-static.py cpu_dma_latency=0
 
     return 0
 }
@@ -13,7 +12,6 @@ start() {
 stop() {
     restore_elevator "/sys/block/{sd,cciss,dm-,vd}*/queue/scheduler"
     restore_cpu_governor
-    /usr/libexec/tuned/pmqos-static.py disable
 
     return 0
 }
