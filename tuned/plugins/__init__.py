e2cf4b13b6a0d94ce720ba4b9aa6b0901b0c0989
Jan Vcelak <jvcelak@redhat.com>
2012-05-11 16:17:20 +0200

    fixup: rfkill base

--- tuned/plugins/__init__.py
+++ tuned/plugins/__init__.py
@@ -1,9 +1,11 @@
 import base
 import repository
+import rfkill
 
-__all__ = ["repository", "get_repository", "Plugin"]
+__all__ = ["repository", "get_repository", "Plugin", "RFKillPlugin"]
 
 def get_repository():
 	return repository.PluginRepository.get_instance()
 
 Plugin = base.Plugin
+RFKillPlugin = rfkill.RFKillPlugin
