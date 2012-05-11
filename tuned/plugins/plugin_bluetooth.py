aa1a737ba044d52e3bc67963dd01b13b693eb8ed
Jan Vcelak <jvcelak@redhat.com>
2012-05-11 16:13:09 +0200

    bluetooth plugin: create new, derive from rfkill base

---
+++ tuned/plugins/plugin_bluetooth.py
@@ -0,0 +1,15 @@
+import tuned.plugins
+import tuned.log
+
+log = tuned.log.get()
+
+class BluetoothPlugin(tuned.plugins.RFKillPlugin):
+	"""
+	Plugin for tuning bluetooth devices.
+	"""
+
+	#
+	# TODO: ticket #24
+	#
+
+	pass
