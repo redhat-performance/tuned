f0107931795f82a84e78f36752237bcc143e7adb
Jan Vcelak <jvcelak@redhat.com>
2012-05-11 16:10:53 +0200

    audio plugin: use new commands interface

--- tuned/plugins/plugin_audio.py
+++ tuned/plugins/plugin_audio.py
@@ -8,8 +8,9 @@ import glob
 
 log = tuned.logs.get()
 
-class VideoPlugin(tuned.plugins.Plugin):
+class AudioPlugin(tuned.plugins.Plugin):
 	"""
+	Plugin for tuning audio cards powersaving options.
 	"""
 
 	@classmethod
@@ -26,38 +27,42 @@ class VideoPlugin(tuned.plugins.Plugin):
 	def update_tuning(self):
 		pass
 
+	def _ac97_powersave_file(self):
+		return "/sys/module/snd_ac97_codec/parameters/power_save"
+
 	@command_set("ac97_powersave")
 	def _set_ac97_powersave(self, value):
-		if value == "1" or value == "true":
-			value = "Y"
-		elif value == "0" or value == "false":
-			value = "N"
-		else:
+		value = self._config_bool(value, "Y", "N")
+		if value is None:
 			log.warn("Incorrect ac97_powersave value.")
 			return
 
-		sys_file = "/sys/module/snd_ac97_codec/parameters/power_save"
+		sys_file = self._ac97_powersave_file()
 		if not os.path.exists(sys_file):
 			return
+
 		tuned.utils.commands.write_to_file(sys_file, value)
 
 	@command_get("ac97_powersave")
 	def _get_ac97_powersave(self, value):
-		sys_file = "/sys/module/snd_ac97_codec/parameters/power_save"
+		sys_file = self._ac97_powersave_file()
 		if not os.path.exists(sys_file):
 			return None
 		return tuned.utils.commands.read_file(sys_file)
 
+	def _hda_intel_powersave_file(self):
+		return "/sys/module/snd_hda_intel/parameters/power_save"
+
 	@command_set("hda_intel_powersave")
 	def _set_hda_intel_powersave(self, value):
-		sys_file = "/sys/module/snd_hda_intel/parameters/power_save"
+		sys_file = self._hda_intel_powersave_file()
 		if not os.path.exists(sys_file):
 			return
 		tuned.utils.commands.write_to_file(sys_file, value)
 
 	@command_get("hda_intel_powersave")
 	def _revert_hda_intel_powersave(self, value):
-		sys_file = "/sys/module/snd_hda_intel/parameters/power_save"
+		sys_file = self._hda_intel_powersave_file()
 		if not os.path.exists(sys_file):
 			return None
 		return tuned.utils.commands.read_file(sys_file)
