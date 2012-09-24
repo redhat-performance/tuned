29f7357f30be809cb97d9a57e73feb9b62ff3909
Jan Vcelak <jvcelak@redhat.com>
2012-09-24 13:46:41 +0200

    storage.factory: add provider property

--- tuned/storage/factory.py
+++ tuned/storage/factory.py
@@ -7,5 +7,9 @@ class Factory(interfaces.Factory):
 	def __init__(self, storage_provider):
 		self._storage_provider = storage_provider
 
+	@property
+	def provider(self):
+		return self._storage_provider
+
 	def create(self, namespace):
 		return storage.Storage(self._storage_provider, namespace)
