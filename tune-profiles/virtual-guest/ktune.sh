64b45e8a04b06c0f554c64b458c590c9904f7f5f
Jan Vcelak <jvcelak@redhat.com>
2012-09-27 13:40:38 +0200

    profiles: do not disable barriers on devices with write back cache
    Resolves: #801561

--- tune-profiles/virtual-guest/ktune.sh
+++ tune-profiles/virtual-guest/ktune.sh
@@ -5,15 +5,7 @@
 start() {
 	set_cpu_governor performance
 	enable_transparent_hugepages
-
-	# Find non-root and non-boot partitions, disable barriers on them
-	rootvol=$(df -h / | grep "^/dev" | awk '{print $1}')
-	bootvol=$(df -h /boot | grep "^/dev" | awk '{print $1}')
-	volumes=$(df -hl --exclude=tmpfs | grep "^/dev" | awk '{print $1}')
-
-	nobarriervols=$(echo "$volumes" | grep -v $rootvol | grep -v $bootvol)
-	remount_partitions nobarrier $nobarriervols
-
+	disable_disk_barriers
 	multiply_disk_readahead 4
 
 	return 0
@@ -22,15 +14,7 @@ start() {
 stop() {
 	restore_cpu_governor
 	restore_transparent_hugepages
-
-	# Find non-root and non-boot partitions, re-enable barriers
-	rootvol=$(df -h / | grep "^/dev" | awk '{print $1}')
-	bootvol=$(df -h /boot | grep "^/dev" | awk '{print $1}')
-	volumes=$(df -hl --exclude=tmpfs | grep "^/dev" | awk '{print $1}')
-
-	nobarriervols=$(echo "$volumes" | grep -v $rootvol | grep -v $bootvol)
-	remount_partitions barrier $nobarriervols
-
+	enable_disk_barriers
 	multiply_disk_readahead 0.25
 
 	return 0
