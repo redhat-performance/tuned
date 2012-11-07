18d0c553fd77223b33b6853e76b42a1477f4178f
Jan Vcelak <jvcelak@redhat.com>
2012-11-07 13:20:58 +0100

    tuned-adm bash completion: complete profiles from all default locations

--- tuned.bash
+++ tuned.bash
@@ -2,15 +2,14 @@
 
 _tuned_adm()
 {
-	local commands="help list active off profile"
+	local commands="active list off profile"
 	local current="${COMP_WORDS[$COMP_CWORD]}"
 	local previous="${COMP_WORDS[$COMP_CWORD-1]}"
 
 	if [[ $COMP_CWORD -eq 1 ]]; then
 		COMPREPLY=( $(compgen -W "$commands" -- "$current" ) )
 	elif [[ $COMP_CWORD -eq 2 && "$previous" == "profile" ]]; then
-		COMPREPLY=( $(compgen -W "$(command ls -F /usr/lib/tuned | \
-			sed '/\/$/!d;s/.$//')" -- "$current" ) )
+		COMPREPLY=( $(compgen -W "$(command find /usr/lib/tuned /etc/tuned -mindepth 1 -maxdepth 1 -type d -printf "%f\n")" -- "$current" ) )
 	else
 		COMPREPLY=()
 	fi
