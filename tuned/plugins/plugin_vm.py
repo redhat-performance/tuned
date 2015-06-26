645ede75b99ff80957d1ddc2ab20efab43090318
Jaroslav Škarvada <jskarvad@redhat.com>
2015-06-26 13:57:49 +0200

    plugin_vm: improved wording of message regarding overridden transparent_hugepage
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- tuned/plugins/plugin_vm.py
+++ tuned/plugins/plugin_vm.py
@@ -45,7 +45,7 @@ class VMPlugin(base.Plugin):
 		cmdline = cmd.read_file("/proc/cmdline", no_error = True)
 		if cmdline.find("transparent_hugepage=") > 0:
 			if not sim:
-				log.info("transparent_hugepage overriden in kernel boot cmdline, ingoring value from profile")
+				log.info("transparent_hugepage is already set in kernel boot cmdline, ingoring value from profile")
 			return None
 
 		sys_file = self._thp_file()
