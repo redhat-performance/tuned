d8b925ae65f38a6ef5b2241083fedf6587258d26
Jaroslav Škarvada <jskarvad@redhat.com>
2017-03-30 15:07:49 +0200

    realtime: fixed misleading error message if no isolated cores are set
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- profiles/realtime/script.sh
+++ profiles/realtime/script.sh
@@ -4,7 +4,7 @@
 
 start() {
     if [ -z "$TUNED_isolated_cores" ]; then
-      echo "no isolated cores set, realtime profile not activating" >&2
+      echo "no isolated cores set, realtime profile not correctly activated" >&2
       exit 1
     fi
 
