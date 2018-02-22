685e16640dc5c9c25037eb97cec41f9df308db46
Luiz Capitulino <lcapitulino@redhat.com>
2018-02-22 13:12:21 -0500

    realtime-virtual-host: script.sh: standardize variable names
    script.sh has been using variable names in different ways,
    in different scopes: when using variables at all. Fix all that.
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>

--- profiles/realtime-virtual-host/script.sh
+++ profiles/realtime-virtual-host/script.sh
@@ -2,8 +2,9 @@
 
 . /usr/lib/tuned/functions
 
-ltanfile=/sys/module/kvm/parameters/lapic_timer_advance_ns
-
+CACHE_VALUE_FILE=./lapic_timer_adv_ns
+CACHE_CPU_FILE=./lapic_timer_adv_ns.cpumodel
+KVM_LAPIC_FILE=/sys/module/kvm/parameters/lapic_timer_advance_ns
 QEMU=$(type -P qemu-kvm || echo /usr/libexec/qemu-kvm)
 TSCDEADLINE_LATENCY="/usr/share/qemu-kvm/tscdeadline_latency.flat"
 if [ ! -f "$TSCDEADLINE_LATENCY" ]; then
@@ -12,15 +13,15 @@ fi
 
 run_tsc_deadline_latency()
 {
-    if [ ! -f /sys/module/kvm/parameters/lapic_timer_advance_ns ]; then
-        echo "/sys/module/kvm/parameters/lapic_timer_advance_ns not found"
+    if [ ! -f $KVM_LAPIC_FILE ]; then
+        echo "$KVM_LAPIC_FILE not found"
         return 1
     fi
 
     dir=`mktemp -d`
 
     for i in `seq 1000 500 7000`; do
-        echo $i > /sys/module/kvm/parameters/lapic_timer_advance_ns
+        echo $i > $KVM_LAPIC_FILE
         chrt -f 1 taskset -c $1 $QEMU -enable-kvm -device pc-testdev \
             -device isa-debug-exit,iobase=0xf4,iosize=0x4 \
             -display none -serial stdio -device pci-testdev \
@@ -48,32 +49,32 @@ start() {
 
     setup_kvm_mod_low_latency
 
-    if [ -f lapic_timer_adv_ns.cpumodel ]; then
+    if [ -f $CACHE_CPU_FILE ]; then
         curmodel=`cat /proc/cpuinfo | grep "model name" | cut -f 2 -d ":" | uniq`
-        genmodel=`cat lapic_timer_adv_ns.cpumodel`
+        genmodel=`cat $CACHE_CPU_FILE`
 
         if [ "$curmodel" != "$genmodel" ]; then
-            rm -f lapic_timer_adv_ns
-            rm -f lapic_timer_adv_ns.cpumodel
+            rm -f $CACHE_VALUE_FILE
+            rm -f $CACHE_CPU_FILE
         fi
     fi
 
 
-    if [ -f $ltanfile -a ! -f ./lapic_timer_adv_ns ]; then
+    if [ -f $KVM_LAPIC_FILE -a ! -f $CACHE_VALUE_FILE ]; then
         if [ -f "$TSCDEADLINE_LATENCY" ]; then
              tempdir=`mktemp -d`
              isolatedcpu=`echo "$TUNED_isolated_cores_expanded" | cut -f 1 -d ","`
              run_tsc_deadline_latency $isolatedcpu > $tempdir/lat.out
              sh ./find-lapictscdeadline-optimal.sh $tempdir/lat.out > $tempdir/opt.out
              if [ $? -eq 0 ]; then
-                  echo `cat $tempdir/opt.out | cut -f 2 -d ":"` > ./lapic_timer_adv_ns
+                  echo `cat $tempdir/opt.out | cut -f 2 -d ":"` > $CACHE_VALUE_FILE
                   curmodel=`cat /proc/cpuinfo | grep "model name" | cut -f 2 -d ":" | uniq`
-                  echo "$curmodel" > lapic_timer_adv_ns.cpumodel
+                  echo "$curmodel" > $CACHE_CPU_FILE
              fi
         fi
     fi
-    if [ -f $ltanfile -a -f ./lapic_timer_adv_ns ]; then
-        echo `cat ./lapic_timer_adv_ns` > $ltanfile
+    if [ -f $KVM_LAPIC_FILE -a -f $CACHE_VALUE_FILE ]; then
+        echo `cat $CACHE_VALUE_FILE` > $KVM_LAPIC_FILE
     fi
 
     disable_ksm
