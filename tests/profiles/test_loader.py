a2ed07c1af1c3af6e2693ab526f3e0d5c1d59d2e
Tomas Korbar <tkorbar@redhat.com>
2018-08-17 10:08:41 +0200

    Fix profiles.Loader class tests

--- tests/profiles/test_loader.py
+++ tests/profiles/test_loader.py
@@ -1,111 +1,114 @@
-import unittest
+import unittest2
+import flexmock
 import tempfile
 import shutil
-import os.path
-import tuned.profiles.exceptions
-from tuned.profiles.loader import Loader
-from flexmock import flexmock
+import os
 
-class MockProfile(object):
-	def __init__(self, name, config):
-		self.name = name
-		self.options = {}
-		self.units = {}
-		self.test_config = config
+import tuned.profiles as profiles
+from tuned.profiles.exceptions import InvalidProfileException
 
-class MockProfileFactory(object):
-	def create(self, name, config):
-		return MockProfile(name, config)
-
-class MockProfileMerger(object):
-	def merge(self, profiles):
-		new = MockProfile("merged", {})
-		new.test_merged = profiles
-		return new
-
-class LoaderTestCase(unittest.TestCase):
-	def setUp(self):
-		self.factory = MockProfileFactory()
-		self.merger = MockProfileMerger()
-		self.loader = Loader(self._tmp_load_dirs, self.factory, self.merger)
-
+class LoaderTestCase(unittest2.TestCase):
 	@classmethod
 	def setUpClass(cls):
-		tmpdir1 = tempfile.mkdtemp()
-		tmpdir2 = tempfile.mkdtemp()
-		cls._tmp_load_dirs = [tmpdir1, tmpdir2]
+		profiles.loader.log = flexmock.flexmock(info = lambda *args: None,\
+			error = lambda *args: None,debug = lambda *args: None,\
+			warn = lambda *args: None)
+		cls._test_dir = tempfile.mkdtemp()
+		cls._profiles_dir = cls._test_dir + '/test_profiles'
+		cls._dummy_profile_dir = cls._profiles_dir + '/dummy'
+		cls._dummy_profile_dir2 = cls._profiles_dir + '/dummy2'
+		cls._dummy_profile_dir3 = cls._profiles_dir + '/dummy3'
+		cls._dummy_profile_dir4 = cls._profiles_dir + '/dummy4'
+		try:
+			os.mkdir(cls._profiles_dir)
+			os.mkdir(cls._dummy_profile_dir)
+			os.mkdir(cls._dummy_profile_dir2)
+			os.mkdir(cls._dummy_profile_dir3)
+			os.mkdir(cls._dummy_profile_dir4)
+		except OSError:
+			pass
 
-		cls._create_profile(tmpdir1, "default", "[main]\n\n[network]\ntype=net\ndevices=em*\n\n[disk]\nenabled=false\n")
-		cls._create_profile(tmpdir1, "invalid", "INVALID")
-		cls._create_profile(tmpdir1, "expand", "[expand]\ntype=script\nscript=runme.sh\n")
-		cls._create_profile(tmpdir2, "empty", "")
+		with open(cls._dummy_profile_dir + '/tuned.conf','w') as f:
+			f.write('[main]\nsummary=dummy profile\n')
+			f.write('[test_unit]\ntest_option=hello\n')
+			f.write('random_option=random\n')
 
-		cls._create_profile(tmpdir1, "custom", "[custom]\ntype=one\n")
-		cls._create_profile(tmpdir2, "custom", "[custom]\ntype=two\n")
+		with open(cls._dummy_profile_dir2 + '/tuned.conf','w') as f:
+			f.write(\
+			'[main]\nsummary=second dummy profile\n')
+			f.write('[test_unit]\ntest_option=hello world\n')
+			f.write('secondary_option=whatever\n')
+
+		with open(cls._dummy_profile_dir3 + '/tuned.conf','w') as f:
+			f.write('[main]\nsummary=another profile\ninclude=dummy\n')
+			f.write('[test_unit]\ntest_option=bye bye\n')
+			f.write('new_option=add this\n')
+
+		with open(cls._dummy_profile_dir4 + '/tuned.conf','w') as f:
+			f.write(\
+				'[main]\nsummary=dummy profile for configuration read test\n')
+			f.write('file_path=${i:PROFILE_DIR}/whatever\n')
+			f.write('script=random_name.sh\n')
+			f.write('[test_unit]\ntest_option=hello world\n')
+
+	def setUp(self):
+		locator = profiles.Locator([self._profiles_dir])
+		factory = profiles.Factory()
+		merger = profiles.Merger()
+		self._loader = profiles.Loader(locator,factory,merger,None,\
+			profiles.variables.Variables())
+
+	def test_safe_name(self):
+		self.assertFalse(self._loader.safe_name('*'))
+		self.assertFalse(self._loader.safe_name('$'))
+		self.assertTrue(self._loader.safe_name('Allowed_ch4rs.-'))
+
+	def test_load_without_include(self):
+		merged_profile = self._loader.load(['dummy','dummy2'])
+
+		self.assertEqual(merged_profile.name, 'dummy dummy2')
+		self.assertEqual(merged_profile.options['summary'],\
+			'second dummy profile')
+		self.assertEqual(merged_profile.units['test_unit'].\
+			options['test_option'],'hello world')
+		self.assertEqual(merged_profile.units['test_unit'].\
+			options['secondary_option'],'whatever')
+
+		with self.assertRaises(InvalidProfileException):
+			self._loader.load([])
+
+		with self.assertRaises(InvalidProfileException):
+			self._loader.load(['invalid'])
+
+	def test_load_with_include(self):
+		merged_profile = self._loader.load(['dummy3'])
+
+		self.assertEqual(merged_profile.name,'dummy3')
+		self.assertEqual(merged_profile.options['summary'],'another profile')
+		self.assertEqual(merged_profile.units['test_unit'].\
+			options['test_option'],'bye bye')
+		self.assertEqual(merged_profile.units['test_unit'].\
+			options['new_option'],'add this')
+		self.assertEqual(merged_profile.units['test_unit'].\
+			options['random_option'],'random')
+
+	def test_expand_profile_dir(self):
+		self.assertEqual(self._loader._expand_profile_dir(\
+			'/hello/world','${i:PROFILE_DIR}/file'),'/hello/world/file')
+
+	def test_load_config_data(self):
+		config = self._loader._load_config_data(\
+			self._dummy_profile_dir4 + '/tuned.conf')
+
+		self.assertEqual(config['main']['script'][0],\
+			self._dummy_profile_dir4 + '/random_name.sh')
+
+		self.assertEqual(config['main']['file_path'],\
+			self._dummy_profile_dir4 + '/whatever')
+
+		self.assertEqual(config['test_unit']['test_option'],\
+			'hello world')
 
 	@classmethod
 	def tearDownClass(cls):
-		for tmp_dir in cls._tmp_load_dirs:
-			shutil.rmtree(tmp_dir, True)
-
-	@classmethod
-	def _create_profile(cls, load_dir, profile_name, tuned_conf_content):
-		profile_dir = os.path.join(load_dir, profile_name)
-		conf_name = os.path.join(profile_dir, "tuned.conf")
-		os.mkdir(profile_dir)
-		with open(conf_name, "w") as conf_file:
-			conf_file.write(tuned_conf_content)
-
-	def test_init(self):
-		Loader([], None, None)
-		Loader(["/tmp"], None, None)
-		Loader(["/foo", "/bar"], None, None)
-
-	def test_init_wrong_type(self):
-		with self.assertRaises(TypeError):
-			Loader(False, self.factory, self.merger)
-
-	def test_load(self):
-		profile = self.loader.load("default")
-		self.assertIn("main", profile.test_config)
-		self.assertIn("disk", profile.test_config)
-		self.assertEqual(profile.test_config["network"]["devices"], "em*")
-
-	def test_load_empty(self):
-		profile = self.loader.load("empty")
-		self.assertDictEqual(profile.test_config, {})
-
-	def test_load_invalid(self):
-		with self.assertRaises(tuned.profiles.exceptions.InvalidProfileException):
-			invalid_config = self.loader.load("invalid")
-
-	def test_load_nonexistent(self):
-		with self.assertRaises(tuned.profiles.exceptions.InvalidProfileException):
-			config = self.loader.load("nonexistent")
-
-	def test_load_order(self):
-		profile = self.loader.load("custom")
-		self.assertEqual(profile.test_config["custom"]["type"], "two")
-
-	def test_default_load(self):
-		profile = self.loader.load("empty")
-		self.assertIs(type(profile), MockProfile)
-
-	def test_script_expand_names(self):
-		profile = self.loader.load("expand")
-		expected_name = os.path.join(self._tmp_load_dirs[0], "expand", "runme.sh")
-		self.assertEqual(profile.test_config["expand"]["script"], expected_name)
-
-	def test_load_multiple_profiles(self):
-		profile = self.loader.load(["default", "expand"])
-		self.assertEqual(len(profile.test_merged), 2)
-
-	def test_include_directive(self):
-		profile1 = MockProfile("first", {})
-		profile1.options = {"include": "default"}
-		profile2 = MockProfile("second", {})
-
-		flexmock(self.factory).should_receive("create").and_return(profile1).and_return(profile2).twice()
-		profile = self.loader.load("empty")
-
-		self.assertEqual(len(profile.test_merged), 2)
+		shutil.rmtree(cls._test_dir)
