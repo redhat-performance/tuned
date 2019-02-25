4331c398433da7820839c9cbe164c7a65ed273b8
Ondřej Lysoněk <olysonek@redhat.com>
2019-02-25 15:44:19 +0100

    sysctl: Don't attempt to set a setting if reading the original fails
    Previously it could (theoretically) happen that reading the original
    value of a setting fails, but then a new value is successfully written.
    This would result in Tuned not being able to restore the original value
    of the setting.
    
    Even though I don't know of a situation where this could happen in
    practice, the code was logically wrong, so let's fix it.
    
    Signed-off-by: Ondřej Lysoněk <olysonek@redhat.com>

--- tuned/plugins/plugin_sysctl.py
+++ tuned/plugins/plugin_sysctl.py
@@ -45,9 +45,16 @@ class SysctlPlugin(base.Plugin):
 	def _instance_apply_static(self, instance):
 		for option, value in list(instance._sysctl.items()):
 			original_value = _read_sysctl(option)
-			if original_value != None:
+			if original_value is None:
+				log.error("sysctl option %s will not be set, failed to read the original value."
+						% option)
+			else:
 				instance._sysctl_original[option] = original_value
-			_write_sysctl(option, self._process_assignment_modifiers(self._variables.expand(self._cmd.unquote(value)), original_value))
+				new_value = self._variables.expand(
+						self._cmd.unquote(value))
+				new_value = self._process_assignment_modifiers(
+						new_value, original_value)
+				_write_sysctl(option, new_value)
 
 		storage_key = self._storage_key(instance.name)
 		self._storage.set(storage_key, instance._sysctl_original)
