76bfc9123190acb73c3cb24b3e61d398ee83a5c0
Ondřej Lysoněk <olysonek@redhat.com>
2020-02-27 15:56:21 +0100

    sysctl: Move sysctl read/apply functions into a class
    Move the functions for reading and applying sysctl settings into a
    class. This will come handy as we start using the new file access API
    in the sysctl plugin - we will be able to store the FileHandler and
    the logger in attributes of the class.
    
    Signed-off-by: Ondřej Lysoněk <olysonek@redhat.com>

--- tuned/plugins/plugin_sysctl/plugin.py
+++ tuned/plugins/plugin_sysctl/plugin.py
@@ -22,6 +22,7 @@ class SysctlPlugin(base.Plugin):
 		super(SysctlPlugin, self).__init__(*args, **kwargs)
 		self._has_dynamic_options = True
 		self._cmd = commands()
+		self._lib = SysctlLib(log)
 
 	def _instance_init(self, instance):
 		instance._has_dynamic_tuning = False
@@ -45,7 +46,7 @@ class SysctlPlugin(base.Plugin):
 
 	def _instance_apply_static(self, instance):
 		for option, value in list(instance._sysctl.items()):
-			original_value = _read_sysctl(option)
+			original_value = self._lib.read_sysctl(option)
 			if original_value is None:
 				log.error("sysctl option %s will not be set, failed to read the original value."
 						% option)
@@ -56,21 +57,21 @@ class SysctlPlugin(base.Plugin):
 						new_value, original_value)
 				if new_value is not None:
 					instance._sysctl_original[option] = original_value
-					_write_sysctl(option, new_value)
+					self._lib.write_sysctl(option, new_value)
 
 		storage_key = self._storage_key(instance.name)
 		self._storage.set(storage_key, instance._sysctl_original)
 
 		if self._global_cfg.get_bool(consts.CFG_REAPPLY_SYSCTL, consts.CFG_DEF_REAPPLY_SYSCTL):
 			log.info("reapplying system sysctl")
-			_apply_system_sysctl()
+			self._lib.apply_system_sysctl()
 
 	def _instance_verify_static(self, instance, ignore_missing, devices):
 		ret = True
 		# override, so always skip missing
 		ignore_missing = True
 		for option, value in list(instance._sysctl.items()):
-			curr_val = _read_sysctl(option)
+			curr_val = self._lib.read_sysctl(option)
 			value = self._process_assignment_modifiers(self._variables.expand(value), curr_val)
 			if value is not None:
 				if self._verify_value(option, self._cmd.remove_ws(value), self._cmd.remove_ws(curr_val), ignore_missing) == False:
@@ -79,103 +80,108 @@ class SysctlPlugin(base.Plugin):
 
 	def _instance_unapply_static(self, instance, full_rollback = False):
 		for option, value in list(instance._sysctl_original.items()):
-			_write_sysctl(option, value)
+			self._lib.write_sysctl(option, value)
 
 
-def _apply_system_sysctl():
-	files = {}
-	for d in SYSCTL_CONFIG_DIRS:
-		try:
-			flist = os.listdir(d)
-		except OSError:
-			continue
-		for fname in flist:
-			if not fname.endswith(".conf"):
+class SysctlLib(object):
+	def __init__(self, logger):
+		self._log = logger
+
+	def apply_system_sysctl(self):
+		files = {}
+		for d in SYSCTL_CONFIG_DIRS:
+			try:
+				flist = os.listdir(d)
+			except OSError:
 				continue
-			if fname not in files:
-				files[fname] = d
+			for fname in flist:
+				if not fname.endswith(".conf"):
+					continue
+				if fname not in files:
+					files[fname] = d
 
-	for fname in sorted(files.keys()):
-		d = files[fname]
-		path = "%s/%s" % (d, fname)
-		_apply_sysctl_config_file(path)
-	_apply_sysctl_config_file("/etc/sysctl.conf")
+		for fname in sorted(files.keys()):
+			d = files[fname]
+			path = "%s/%s" % (d, fname)
+			self._apply_sysctl_config_file(path)
+		self._apply_sysctl_config_file("/etc/sysctl.conf")
 
-def _apply_sysctl_config_file(path):
-	log.debug("Applying sysctl settings from file %s" % path)
-	try:
-		with open(path, "r") as f:
-			for lineno, line in enumerate(f, 1):
-				_apply_sysctl_config_line(path, lineno, line)
-		log.debug("Finished applying sysctl settings from file %s"
-				% path)
-	except (OSError, IOError) as e:
-		if e.errno != errno.ENOENT:
-			log.error("Error reading sysctl settings from file %s: %s"
-					% (path, str(e)))
+	def _apply_sysctl_config_file(self, path):
+		self._log.debug("Applying sysctl settings from file %s" % path)
+		try:
+			with open(path, "r") as f:
+				for lineno, line in enumerate(f, 1):
+					self._apply_sysctl_config_line(path, lineno, line)
+			self._log.debug("Finished applying sysctl settings from file %s"
+					% path)
+		except (OSError, IOError) as e:
+			if e.errno != errno.ENOENT:
+				self._log.error("Error reading sysctl settings from file %s: %s"
+						% (path, str(e)))
 
-def _apply_sysctl_config_line(path, lineno, line):
-	line = line.strip()
-	if len(line) == 0 or line[0] == "#" or line[0] == ";":
-		return
-	tmp = line.split("=", 1)
-	if len(tmp) != 2:
-		log.error("Syntax error in file %s, line %d"
-				% (path, lineno))
-		return
-	option, value = tmp
-	option = option.strip()
-	if len(option) == 0:
-		log.error("Syntax error in file %s, line %d"
-				% (path, lineno))
-		return
-	value = value.strip()
-	_write_sysctl(option, value, ignore_missing = True)
+	def _apply_sysctl_config_line(self, path, lineno, line):
+		line = line.strip()
+		if len(line) == 0 or line[0] == "#" or line[0] == ";":
+			return
+		tmp = line.split("=", 1)
+		if len(tmp) != 2:
+			self._log.error("Syntax error in file %s, line %d"
+					% (path, lineno))
+			return
+		option, value = tmp
+		option = option.strip()
+		if len(option) == 0:
+			self._log.error("Syntax error in file %s, line %d"
+					% (path, lineno))
+			return
+		value = value.strip()
+		self.write_sysctl(option, value, ignore_missing = True)
 
-def _get_sysctl_path(option):
-	return "/proc/sys/%s" % option.replace(".", "/")
+	@staticmethod
+	def _get_sysctl_path(option):
+		return "/proc/sys/%s" % option.replace(".", "/")
 
-def _read_sysctl(option):
-	path = _get_sysctl_path(option)
-	try:
-		with open(path, "r") as f:
-			line = ""
-			for i, line in enumerate(f):
-				if i > 0:
-					log.error("Failed to read sysctl parameter '%s', multi-line values are unsupported"
-							% option)
-					return None
-			value = line.strip()
-		log.debug("Value of sysctl parameter '%s' is '%s'"
-				% (option, value))
-		return value
-	except (OSError, IOError) as e:
-		if e.errno == errno.ENOENT:
-			log.error("Failed to read sysctl parameter '%s', the parameter does not exist"
-					% option)
-		else:
-			log.error("Failed to read sysctl parameter '%s': %s"
-					% (option, str(e)))
-		return None
-
-def _write_sysctl(option, value, ignore_missing = False):
-	path = _get_sysctl_path(option)
-	if os.path.basename(path) in DEPRECATED_SYSCTL_OPTIONS:
-		log.error("Refusing to set deprecated sysctl option %s"
-				% option)
-		return False
-	try:
-		log.debug("Setting sysctl parameter '%s' to '%s'"
-				% (option, value))
-		with open(path, "w") as f:
-			f.write(value)
-		return True
-	except (OSError, IOError) as e:
-		if e.errno == errno.ENOENT:
-			log_func = log.debug if ignore_missing else log.error
-			log_func("Failed to set sysctl parameter '%s' to '%s', the parameter does not exist"
+	def read_sysctl(self, option):
+		path = self._get_sysctl_path(option)
+		try:
+			with open(path, "r") as f:
+				line = ""
+				for i, line in enumerate(f):
+					if i > 0:
+						self._log.error("Failed to read sysctl parameter '%s', multi-line values are unsupported"
+								% option)
+						return None
+				value = line.strip()
+			self._log.debug("Value of sysctl parameter '%s' is '%s'"
 					% (option, value))
-		else:
-			log.error("Failed to set sysctl parameter '%s' to '%s': %s"
-					% (option, value, str(e)))
-		return False
+			return value
+		except (OSError, IOError) as e:
+			if e.errno == errno.ENOENT:
+				self._log.error("Failed to read sysctl parameter '%s', the parameter does not exist"
+						% option)
+			else:
+				self._log.error("Failed to read sysctl parameter '%s': %s"
+						% (option, str(e)))
+			return None
+
+	def write_sysctl(self, option, value, ignore_missing = False):
+		path = self._get_sysctl_path(option)
+		if os.path.basename(path) in DEPRECATED_SYSCTL_OPTIONS:
+			self._log.error("Refusing to set deprecated sysctl option %s"
+					% option)
+			return False
+		try:
+			self._log.debug("Setting sysctl parameter '%s' to '%s'"
+					% (option, value))
+			with open(path, "w") as f:
+				f.write(value)
+			return True
+		except (OSError, IOError) as e:
+			if e.errno == errno.ENOENT:
+				log_func = self._log.debug if ignore_missing else self._log.error
+				log_func("Failed to set sysctl parameter '%s' to '%s', the parameter does not exist"
+						% (option, value))
+			else:
+				self._log.error("Failed to set sysctl parameter '%s' to '%s': %s"
+						% (option, value, str(e)))
+			return False
