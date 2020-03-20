5ed61ce394bad089f86a0be4b911cd93eddeb1b3
Jaroslav Škarvada <jskarvad@redhat.com>
2020-03-20 18:02:46 +0100

    realtime: added conditional support for managed_irq
    Also added regex_search_ternary built-in function.
    
    It takes arguments in the following form:
      STR1, REGEX, STR2, STR3
    
    If REGEX matches STR1 (re.search is used), STR2 is returned,
    if it doesn't match STR3 is returned.
    
    Example:
    [variables]
    foo=Y
    bar=${f:regex_search_ternary:${foo}:\b[y,Y,1,t,T]\b:foo:bar}
    
    It will result in the 'foo' string stored in the '${bar}' variable.
    
    Resolves: rhbz#1797025
    
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

---
+++ tuned/profiles/functions/function_regex_search_ternary.py
@@ -0,0 +1,21 @@
+import re
+from . import base
+
+class regex_search_ternary(base.Function):
+	"""
+	Ternary regex operator, it takes arguments in the following form
+	STR1, REGEX, STR2, STR3
+	If REGEX matches STR1 (re.search is used), STR2 is returned,
+	otherwise STR3 is returned
+	"""
+	def __init__(self):
+		# 4 arguments
+		super(regex_search_ternary, self).__init__("regex_search_ternary", 4)
+
+	def execute(self, args):
+		if not super(regex_search_ternary, self).execute(args):
+			return None
+		if re.search(args[1], args[0]):
+			return args[2]
+		else:
+			return args[3]
