7aa8ffe7dd216766d92d81674b42ae3a22c3e795
Courtney Pacheco <cpacheco@cpacheco.bos.csb>
2020-12-15 09:21:12 -0500

    Added a docstring to the sysctl plugin.
    Adding a docstring to the sysctl plugin that describes what the
    plugin does, what its parameters are, and how it should be used.

--- tuned/plugins/plugin_sysctl.py
+++ tuned/plugins/plugin_sysctl.py
@@ -16,7 +16,12 @@ SYSCTL_CONFIG_DIRS = [ "/run/sysctl.d",
 
 class SysctlPlugin(base.Plugin):
 	"""
-	Plugin for applying custom sysctl options.
+	This plugin is used for applying custom `sysctl` settings and should only be used to change 
+        system settings that are not covered by other plugins available in *Tuned*. If the settings
+        are covered by other *Tuned* plugins, please use those plugins instead.
+        
+        The syntax for this plugin is `name=value`, where _name_ is the same as the name provided by
+        the `sysctl` utility.
 	"""
 
 	def __init__(self, *args, **kwargs):
