00a9d5cde4a9ad2d6e71f8ed5259881695bf4c69
Robin Hack <rhack@redhat.com>
2021-10-04 08:00:59 -0400

    Tests: Be in sync with upstreaming guide.

--- tests/beakerlib/tuned-adm-functionality/runtest.sh
+++ tests/beakerlib/tuned-adm-functionality/runtest.sh
@@ -8,21 +8,9 @@
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-#   Copyright (c) 2012 Red Hat, Inc. All rights reserved.
+#   Copyright Red Hat
 #
-#   This copyrighted material is made available to anyone wishing
-#   to use, modify, copy, or redistribute it subject to the terms
-#   and conditions of the GNU General Public License version 2.
-#
-#   This program is distributed in the hope that it will be
-#   useful, but WITHOUT ANY WARRANTY; without even the implied
-#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-#   PURPOSE. See the GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public
-#   License along with this program; if not, write to the Free
-#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-#   Boston, MA 02110-1301, USA.
+#   SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
