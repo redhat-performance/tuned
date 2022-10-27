efbe574c8c6b1f9f9f57a9dd967d0b96c55b0c70
lilinjie <lilinjie@uniontech.com>
2022-10-27 13:40:31 +0800

    update
    Signed-off-by: lilinjie <lilinjie@uniontech.com>

--- tuned/admin/admin.py
+++ tuned/admin/admin.py
@@ -132,6 +132,30 @@ class Admin(object):
 			manual = profile is not None
 		return consts.ACTIVE_PROFILE_MANUAL if manual else consts.ACTIVE_PROFILE_AUTO
 
+	def _action_profile_delete(self, profile = ""):
+		if profile == "":
+			print("No set profile to delete.")
+			return False
+		try:
+			profile_list = os.listdir(consts.USER_PROFILE_DIR)
+			if profile not in profile_list:
+				print("File '%s' does not exists." % profile)
+				return False
+			else:
+				shutil.rmtree(os.path.join(consts.USER_PROFILE_DIR, profile))
+				print("Profile '%s' delete successfully" % profile)
+			
+		except TunedException as e:
+			self._error(str(e))
+			return False
+		return True
+
+	def _dbus_action_profile_delete(self, profile = ""):
+		if profile != "":
+			ret = self._controller.profile_delete(profile)
+			res = True
+		return self._controller.exit(res)
+
 	def _dbus_get_post_loaded_profile(self):
 		profile_name = self._controller.post_loaded_profile()
 		if profile_name == "":
@@ -179,29 +203,6 @@ class Admin(object):
 				return False
 		return self._print_profile_info(profile, self._profiles_locator.get_profile_attrs(profile, [consts.PROFILE_ATTR_SUMMARY, consts.PROFILE_ATTR_DESCRIPTION], ["", ""]))
 
-        def _action_profile_delete(self, profile = ""):
-                if profile == "":
-                        print("No set profile to delete.")
-                        return False
-                try:
-                        profile_list = os.listdir(consts.USER_PROFILE_DIR)
-                        if profile not in profile_list:
-                                print("File '%s' does not exists." % profile)
-                                return False
-                        else:
-                                shutil.rmtree(os.path.join(consts.USER_PROFILE_DIR, profile))
-				print("Profile '%s' delete successfully" % profile)
-
-                except TunedException as e:
-                        self._error(str(e))
-                        return False
-                return True
-
-        def _dbus_action_profile_delete(self, profile = ""):
-                if profile != "":
-                        ret = self._controller.profile_delete(profile)
-                return self._controller.exit(ret)
-		
 	def _print_profile_name(self, profile_name):
 		if profile_name is None:
 			print("No current active profile.")
