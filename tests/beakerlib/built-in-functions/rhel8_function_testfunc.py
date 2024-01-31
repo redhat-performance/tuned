eb5681edeb2182d163ea7a3bbb1191521419fc56
Robin Hack <rhack@redhat.com>
2024-01-31 07:13:07 -0500

    tests/beakerlib: Add new test which covers build-in functions.

---
+++ tests/beakerlib/built-in-functions/rhel8_function_testfunc.py
@@ -0,0 +1,16 @@
+from . import base
+from tuned.utils.commands import commands
+
+class testfunc(base.Function):
+    """
+    Test function
+    """
+    def __init__(self):
+        super(self.__class__, self).__init__("testfunc", 2)
+
+    def execute(self, args):
+        f = open(str(args[0]),'w')
+        f.write(str(args[1]))
+        f.close()
+        return str(args[1]) + 'returned'
+
