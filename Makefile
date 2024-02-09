redhat-performance/tuned

commit 3e03b8b6caf7e759aa985746f83834615854e3aa

Configure the PPD-to-TuneD daemon to work as a replacement of PPD
This includes the configuration of DBus, systemd, Polkit, and a new
TuneD Fedora/RHEL subpackage.

Makefile
@@ -223,6 +223,14 @@ install: install-dirs
 	install -dD $(DESTDIR)$(DATADIR)/applications
 	desktop-file-install --dir=$(DESTDIR)$(DATADIR)/applications tuned-gui.desktop
 
+install-ppd: install
+	$(call install_python_script,tuned-ppd.py,$(DESTDIR)/usr/sbin/tuned-ppd)
+	install -Dpm 0644 tuned/ppd/tuned-ppd.service $(DESTDIR)$(UNITDIR)/tuned-ppd.service
+	install -Dpm 0644 tuned/ppd/tuned-ppd.dbus.service $(DESTDIR)$(DATADIR)/dbus-1/system-services/net.hadess.PowerProfiles.service
+	install -Dpm 0644 tuned/ppd/dbus.conf $(DESTDIR)$(DATADIR)/dbus-1/system.d/net.hadess.PowerProfiles.conf
+	install -Dpm 0644 tuned/ppd/tuned-ppd.policy $(DESTDIR)$(DATADIR)/polkit-1/actions/net.hadess.PowerProfiles.policy
+	install -Dpm 0644 tuned/ppd/ppd.conf $(DESTDIR)$(SYSCONFDIR)/tuned/ppd.conf
+
 clean: clean-html
 	find -name "*.pyc" | xargs rm -f
 	rm -rf $(VERSIONED_NAME) rpm-build-dir

tuned.spec
@@ -264,6 +264,17 @@ Requires: %{name} = %{version}
 %description profiles-openshift
 Additional TuneD profile(s) optimized for OpenShift.
 
+%package ppd
+Summary: PPD compatibility daemon
+Requires: %{name} = %{version}
+# The compatibility daemon is swappable for power-profiles-daemon
+Provides: ppd-service
+Conflicts: ppd-service
+
+%description ppd
+An API translation daemon that allows applications to easily transition
+to TuneD from power-profiles-daemon (PPD).
+
 %prep
 %autosetup -p1 -n %{name}-%{version}%{?prerel2}
 
@@ -276,6 +287,7 @@ make html %{make_python_arg}
 
 %install
 make install DESTDIR=%{buildroot} DOCDIR=%{docdir} %{make_python_arg}
+make install-ppd DESTDIR=%{buildroot} DOCDIR=%{docdir} %{make_python_arg}
 %if 0%{?rhel}
 sed -i 's/\(dynamic_tuning[ \t]*=[ \t]*\).*/\10/' %{buildroot}%{_sysconfdir}/tuned/tuned-main.conf
 %endif
@@ -555,6 +567,14 @@ fi
 %{_prefix}/lib/tuned/openshift-control-plane
 %{_prefix}/lib/tuned/openshift-node
 %{_mandir}/man7/tuned-profiles-openshift.7*
+
+%files ppd
+%{_sbindir}/tuned-ppd
+%{_unitdir}/tuned-ppd.service
+%{_datadir}/dbus-1/system-services/net.hadess.PowerProfiles.service
+%{_datadir}/dbus-1/system.d/net.hadess.PowerProfiles.conf
+%{_datadir}/polkit-1/actions/net.hadess.PowerProfiles.policy
+%config(noreplace) %{_sysconfdir}/tuned/ppd.conf
 
 %changelog
 * Tue Aug 29 2023 Jaroslav Škarvada <jskarvad@redhat.com> - 2.21.0-1

tuned/ppd/dbus.conf
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE busconfig PUBLIC "-//freedesktop//DTD D-BUS Bus Configuration 1.0//EN"
+  "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+
+<busconfig>
+  <policy user="root">
+    <allow own="net.hadess.PowerProfiles"/>
+  </policy>
+
+  <policy context="default">
+    <allow send_destination="net.hadess.PowerProfiles" send_interface="net.hadess.PowerProfiles"/>
+    <allow send_destination="net.hadess.PowerProfiles" send_interface="org.freedesktop.DBus.Introspectable"/>
+    <allow send_destination="net.hadess.PowerProfiles" send_interface="org.freedesktop.DBus.Properties"/>
+    <allow send_destination="net.hadess.PowerProfiles" send_interface="org.freedesktop.DBus.Peer"/>
+  </policy>
+</busconfig>

tuned/ppd/ppd.conf
@@ -1,0 +1,9 @@
+[main]
+# The default PPD profile
+default=balanced
+
+[profiles]
+# PPD = TuneD
+power-saver=powersave
+balanced=balanced
+performance=throughput-performance

tuned/ppd/tuned-ppd.dbus.service
@@ -1,0 +1,5 @@
+[D-BUS Service]
+Name=net.hadess.PowerProfiles
+Exec=/bin/false
+User=root
+SystemdService=tuned-ppd.service

tuned/ppd/tuned-ppd.policy
@@ -1,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE policyconfig PUBLIC "-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN"
+"http://www.freedesktop.org/standards/PolicyKit/1.0/policyconfig.dtd">
+<policyconfig>
+
+  <vendor>TuneD</vendor>
+  <vendor_url>https://tuned-project.org/</vendor_url>
+
+  <action id="net.hadess.PowerProfiles.HoldProfile">
+    <description>Hold power profile</description>
+    <message>Authentication is required to hold power profiles.</message>
+    <defaults>
+      <allow_any>no</allow_any>
+      <allow_inactive>no</allow_inactive>
+      <allow_active>yes</allow_active>
+    </defaults>
+  </action>
+  
+  <action id="net.hadess.PowerProfiles.ReleaseProfile">
+    <description>Release power profile</description>
+    <message>Authentication is required to release power profiles.</message>
+    <defaults>
+      <allow_any>no</allow_any>
+      <allow_inactive>no</allow_inactive>
+      <allow_active>yes</allow_active>
+    </defaults>
+  </action>
+
+</policyconfig>

tuned/ppd/tuned-ppd.service
@@ -1,0 +1,14 @@
+[Unit]
+Description=PPD-to-TuneD API Translation Daemon
+Requires=tuned.service
+After=tuned.service
+Before=multi-user.target display-manager.target
+
+[Service]
+Type=dbus
+PIDFile=/run/tuned/tuned-ppd.pid
+BusName=net.hadess.PowerProfiles
+ExecStart=/usr/sbin/tuned-ppd
+
+[Install]
+WantedBy=graphical.target

commit 1883671d60b6e919f9d0fcb40f0c9570342a9403

Add missing install command for tuned-ppd.py
Fixes: 3e03b8b Configure the PPD-to-TuneD daemon to work as a replacement of PPD
Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
Closes: https://github.com/redhat-performance/tuned/issues/597

Makefile
@@ -66,7 +66,7 @@ release-cp: release-dir
 	cp -a tuned.py tuned.spec tuned.service tuned.tmpfiles Makefile tuned-adm.py \
 		tuned-adm.bash dbus.conf recommend.conf tuned-main.conf 00_tuned \
 		92-tuned.install bootcmdline modules.conf com.redhat.tuned.policy \
-		tuned-gui.py tuned-gui.glade \
+		tuned-gui.py tuned-gui.glade tuned-ppd.py \
 		tuned-gui.desktop $(VERSIONED_NAME)
 	cp -a doc experiments libexec man profiles systemtap tuned contrib icons \
 		tests $(VERSIONED_NAME)