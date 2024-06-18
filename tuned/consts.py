redhat-performance/tuned

commit 6d3aa949b3a7019bbb70f9bd3b34cb4a9381e8fb

Migrate profiles to /etc/tuned/profiles/ and /usr/lib/tuned/profiles/

tuned/consts.py
@@ -13,7 +13,7 @@ DBUS_INTERFACE = "com.redhat.tuned.control"
 DBUS_OBJECT = "/Tuned"
 DEFAULT_PROFILE = "balanced"
 DEFAULT_STORAGE_FILE = "/run/tuned/save.pickle"
-SYSTEM_PROFILE_DIR = "/usr/lib/tuned"
+SYSTEM_PROFILE_DIR = "/usr/lib/tuned/profiles"
 PERSISTENT_STORAGE_DIR = "/var/lib/tuned"
 PLUGIN_MAIN_UNIT_NAME = "main"
 # Magic section header because ConfigParser does not support "headerless" config
@@ -173,7 +173,7 @@ CFG_FUNC_UNIX_SOCKET_CONNECTIONS_BACKLOG = "getint"
 # default rollback strategy
 CFG_DEF_ROLLBACK = "auto"
 # default profile directories
-CFG_DEF_PROFILE_DIRS = [SYSTEM_PROFILE_DIR, "/etc/tuned"]
+CFG_DEF_PROFILE_DIRS = [SYSTEM_PROFILE_DIR, "/etc/tuned/profiles"]
 
 PATH_CPU_DMA_LATENCY = "/dev/cpu_dma_latency"
 

Makefile
@@ -46,7 +46,7 @@ ifeq ($(PYTHON_SITELIB),)
 $(error Failed to determine python library directory)
 endif
 KERNELINSTALLHOOKDIR = /usr/lib/kernel/install.d
-TUNED_PROFILESDIR = /usr/lib/tuned
+TUNED_PROFILESDIR = /usr/lib/tuned/profiles
 TUNED_RECOMMEND_DIR = $(TUNED_PROFILESDIR)/recommend.d
 TUNED_USER_RECOMMEND_DIR = $(SYSCONFDIR)/tuned/recommend.d
 BASH_COMPLETIONS = $(DATADIR)/bash-completion/completions
@@ -134,6 +134,7 @@ install-dirs:
 	mkdir -p $(DESTDIR)/run/tuned
 	mkdir -p $(DESTDIR)$(DOCDIR)
 	mkdir -p $(DESTDIR)$(SYSCONFDIR)
+	mkdir -p $(DESTDIR)$(SYSCONFDIR)/tuned/profiles
 	mkdir -p $(DESTDIR)$(TUNED_RECOMMEND_DIR)
 	mkdir -p $(DESTDIR)$(TUNED_USER_RECOMMEND_DIR)
 

man/tuned-adm.8
@@ -31,8 +31,8 @@ This command line utility allows you to switch between user definable tuning
 profiles. Several predefined profiles are already included. You can even
 create your own profile, either based on one of the existing ones by copying
 it or make a completely new one. The distribution provided profiles are stored
-in subdirectories below \fI/usr/lib/tuned\fP and the user defined profiles in
-subdirectories below \fI/etc/tuned\fP. If there are profiles with the same name
+in subdirectories below \fI/usr/lib/tuned/profiles/\fP and the user defined profiles in
+subdirectories below \fI/etc/tuned/profiles/\fP. If there are profiles with the same name
 in both places, user defined profiles have precedence.
 
 .SH "OPTIONS"

man/tuned-profiles.7
@@ -30,9 +30,9 @@ performance optimizations but there are also profiles targeted to
 low power consumption, low latency and others. You can mostly deduce the
 purpose of the profile by its name or you can see full description below.
 
-The profiles are stored in subdirectories below \fI/usr/lib/tuned\fP. If you
-need to customize the profiles, you can copy them to \fI/etc/tuned\fP and modify
-them as you need. When loading profiles with the same name, the /etc/tuned takes
+The profiles are stored in subdirectories below \fI/usr/lib/tuned/profiles/\fP. If you
+need to customize the profiles, you can copy them to \fI/etc/tuned/profiles/\fP and modify
+them as you need. When loading profiles with the same name, \fI/etc/tuned/profiles/\fP takes
 precedence. In such case you will not lose your customized profiles between
 TuneD updates.
 
@@ -143,8 +143,8 @@ throughput\-performance profile.
 
 .SH "FILES"
 .nf
-.I /etc/tuned/*
-.I /usr/lib/tuned/*
+.I /etc/tuned/profiles/*
+.I /usr/lib/tuned/profiles/*
 
 .SH "SEE ALSO"
 .BR tuned (8)

man/tuned.conf.5
@@ -3,8 +3,8 @@
 tuned.conf - TuneD profile definition
 .SH DESCRIPTION
 This man page documents format of TuneD 2.0 profile definition files.
-The profile definition is stored in /etc/tuned/<profile_name>/tuned.conf or in
-/usr/lib/tuned/<profile_name>/tuned.conf file where the /etc/tuned/ directory has 
+The profile definition is stored in /etc/tuned/profiles/<profile_name>/tuned.conf or in
+/usr/lib/tuned/profiles/<profile_name>/tuned.conf file where the /etc/tuned/profiles/ directory has
 higher priority.
 
 The \fBtuned.conf\fR configures the profile and it is in ini-file format.

tests/beakerlib/Traceback-caused-by-scheduler-plugin-with/runtest.sh
@@ -18,6 +18,7 @@
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 PACKAGE="tuned"
+PROFILE_DIR="/etc/tuned/profiles"
 
 rlJournalStart
     rlPhaseStartSetup
@@ -31,7 +32,7 @@ rlJournalStart
 	rlFileBackup "/etc/tuned/active_profile"
         rlFileBackup "/etc/tuned/profile_mode"
 
-        rlRun "mkdir /etc/tuned/test-profile"
+        rlRun "mkdir $PROFILE_DIR/test-profile"
         rlServiceStart "tuned"
         sleep 1
 	rlFileBackup "/var/log/tuned/tuned.log"
@@ -39,7 +40,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest
-	rlRun "pushd /etc/tuned/test-profile"
+	rlRun "pushd $PROFILE_DIR/test-profile"
 	cat << EOF > tuned.conf
 [scheduler]
 runtime=0
@@ -67,7 +68,7 @@ EOF
 	rlFileRestore
 
         rlServiceRestore "tuned"
-	rlRun "rm -rf /etc/tuned/test-profile"
+	rlRun "rm -rf $PROFILE_DIR/test-profile"
     rlPhaseEnd
 
 rlJournalPrintText

tests/beakerlib/bz1798183-RFE-support-post-loaded-profile/runtest.sh
@@ -18,10 +18,11 @@
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 PACKAGE="tuned"
-PROFILE_DIR=/etc/tuned
-ACTIVE_PROFILE=/etc/tuned/active_profile
-PROFILE_MODE=/etc/tuned/profile_mode
-POST_LOADED_PROFILE=/etc/tuned/post_loaded_profile
+SYSCONF_DIR=/etc/tuned
+PROFILE_DIR=$SYSCONF_DIR/profiles
+ACTIVE_PROFILE=$SYSCONF_DIR/active_profile
+PROFILE_MODE=$SYSCONF_DIR/profile_mode
+POST_LOADED_PROFILE=$SYSCONF_DIR/post_loaded_profile
 SWAPPINESS=vm.swappiness
 DIRTY_RATIO=vm.dirty_ratio
 PID_FILE=/run/tuned/tuned.pid
@@ -64,7 +65,7 @@ rlJournalStart
         rlImport "tuned/basic"
         tunedDisableSystemdRateLimitingStart
         rlRun "for PYTHON in $PYTHON_CHECK; do \$PYTHON --version 2>/dev/null && break; done" 0 "Detect python"
-        rlRun "rlFileBackup --clean $PROFILE_DIR"
+        rlRun "rlFileBackup --clean $SYSCONF_DIR"
         rlRun "cp -r parent $PROFILE_DIR"
         rlRun "cp -r parent2 $PROFILE_DIR"
         rlRun "cp -r parent-vars $PROFILE_DIR"

tests/beakerlib/bz2071418-TuneD-exits-on-duplicate-config-lines-new/runtest.sh
@@ -18,6 +18,7 @@
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 PACKAGE="tuned"
+PROFILE_DIR=/etc/tuned/profiles
 
 rlJournalStart
     rlPhaseStartSetup
@@ -28,8 +29,8 @@ rlJournalStart
         rlServiceStart "tuned"
         tunedProfileBackup
 
-        rlRun "mkdir /etc/tuned/test-profile"
-        rlRun "pushd /etc/tuned/test-profile"
+        rlRun "mkdir $PROFILE_DIR/test-profile"
+        rlRun "pushd $PROFILE_DIR/test-profile"
         cat << EOF > tuned.conf
 [sysctl]
 kernel.sem = 1250 256000 100 8192
@@ -63,7 +64,7 @@ EOF
         tunedProfileRestore
         rlServiceRestore "tuned"
 
-        rlRun "rm -rf /etc/tuned/test-profile"
+        rlRun "rm -rf $PROFILE_DIR/test-profile"
     rlPhaseEnd
 rlJournalPrintText
 rlJournalEnd

commit a2521b4d3074d2be1711764c523876c50371b0ab

update RECOMMEND_DIRECTORIES const for profile migration
The installation was updated, but the consts were missing.

Fixes: 6d3aa949b3a7019bbb70f9bd3b34cb4a9381e8fb

tuned/consts.py
@@ -18,7 +18,7 @@ PERSISTENT_STORAGE_DIR = "/var/lib/tuned"
 PLUGIN_MAIN_UNIT_NAME = "main"
 # Magic section header because ConfigParser does not support "headerless" config
 MAGIC_HEADER_NAME = "this_is_some_magic_section_header_because_of_compatibility"
-RECOMMEND_DIRECTORIES = ["/usr/lib/tuned/recommend.d", "/etc/tuned/recommend.d"]
+RECOMMEND_DIRECTORIES = ["/usr/lib/tuned/profiles/recommend.d", "/etc/tuned/recommend.d"]
 
 TMP_FILE_SUFFIX = ".tmp"
 # max. number of consecutive errors to give up