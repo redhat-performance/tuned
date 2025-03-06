91479c5172b06fa8a0e305ca317f88c2a69185f0
Adriaan Schmidt <adriaan.schmidt@siemens.com>
2025-03-06 08:20:29 +0100

    feat: persistence of dynamic tuning changes
    Currently, any changes made to the tuning via the `instance_*` dbus calls
    are lost when tuning is stopped by the service, or when the TuneD service
    itself is stopped/restarted, or when the service crashes.
    
    This commit:
    
    * implements a sync of Plugin Instances and Profile Units that is currently missing.
      This way, dynamic instances and device assignments are persistent across stop/start
      dbus calls to TuneD.
    * calculates a hash of the current profile after loading it from disk
      (after processing all includes, so we have a "flat" representation)
    * creates snapshots of the current profile whenever instances or assigned
      devices change. the snapshot includes the hash of the profile as
      it was initially loaded. for each instance it stores the devices that
      are currently attached.
    * restores a snapshot found at startup, if the hashes match (i.e. there
      have been no profile switches and no changes to the profile or any of
      its includes on disk)
    
      snapshots are restored in case of
      - daemon restarts (systemctl restart/stop/start)
      - daemon crashes
      snapshots are NOT restored in case of
      - reboots (snapshots are stored in /var/run)
      - profile changes (snapshots are explicitly deleted when switching profiles,
        even when "switching" to the same/current profile)
    
    Signed-off-by: Adriaan Schmidt <adriaan.schmidt@siemens.com>

--- tuned/profiles/factory.py
+++ tuned/profiles/factory.py
@@ -1,5 +1,5 @@
 import tuned.profiles.profile
 
 class Factory(object):
-	def create(self, name, config):
-		return tuned.profiles.profile.Profile(name, config)
+	def create(self, name, config, variables):
+		return tuned.profiles.profile.Profile(name, config, variables)
