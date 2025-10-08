2145e1984b7a1c1736b0f59c01deaefeeedc6cff
Jaroslav Škarvada <jskarvad@redhat.com>
2025-10-08 19:29:41 +0200

    cpu-partitioning: autodetect dracut hook directory, systemd workaround
    Wrong hook directory can make the machine unbootable:
    https://issues.redhat.com/browse/RHEL-119889
    
    Also added workaround for currently non-functional systemd plugin
    which doesn't include the systemd config into the initrd image.
    
    Resolves: RHEL-40619
    
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- profiles/cpu-partitioning/script.sh
+++ profiles/cpu-partitioning/script.sh
@@ -3,10 +3,19 @@
 . /usr/lib/tuned/functions
 
 start() {
+    DRACUT_VER=`dracut --version | sed 's/^.* \([0-9]\+\).*/\1/'`
+    echo "$DRACUT_VER" | grep -q '^[[:digit:]]\+$' || DRACUT_VER="0"
+    # https://issues.redhat.com/browse/RHEL-119889
+    if [ "$DRACUT_VER" -gt "102" ]
+    then
+      DRACUT_HOOK_DIR="/var/lib/dracut/hooks/pre-udev"
+    else
+      DRACUT_HOOK_DIR="/usr/lib/dracut/hooks/pre-udev"
+    fi
     mkdir -p "${TUNED_tmpdir}/etc/systemd"
-    mkdir -p "${TUNED_tmpdir}/usr/lib/dracut/hooks/pre-udev"
+    mkdir -p "${TUNED_tmpdir}${DRACUT_HOOK_DIR}"
     cp /etc/systemd/system.conf "${TUNED_tmpdir}/etc/systemd/"
-    cp 00-tuned-pre-udev.sh "${TUNED_tmpdir}/usr/lib/dracut/hooks/pre-udev/"
+    cp 00-tuned-pre-udev.sh "${TUNED_tmpdir}${DRACUT_HOOK_DIR}"
     setup_kvm_mod_low_latency
     disable_ksm
     return "$?"
