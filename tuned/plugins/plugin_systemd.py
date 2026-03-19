47395d84fb7b86605ff5d66cb0e2f27916214bf2
Fothsid <18036123+Fothsid@users.noreply.github.com>
2026-03-19 16:53:38 +0100

    systemd: do not backup the drop-in configuration
    Resolves: RHEL-84365

--- tuned/plugins/plugin_systemd.py
+++ tuned/plugins/plugin_systemd.py
@@ -19,6 +19,8 @@ class SystemdPlugin(base.Plugin):
 	affinity for the service manager as well as the default CPU affinity
 	for all forked off processes. The option takes a comma-separated
 	list of CPUs with optional CPU ranges specified by the minus sign (`-`).
+	Please avoid changing the drop-in configuration file, as it is not
+	backed up and will be overwritten.
 
 	.Set the CPUAffinity for `systemd` to `0 1 2 3`
 	====
@@ -99,19 +101,10 @@ class SystemdPlugin(base.Plugin):
 			return False
 		return True
 
-	def _get_storage_filename(self):
-		return os.path.join(consts.PERSISTENT_STORAGE_DIR, self.name)
-
 	def _remove_systemd_tuning(self):
 		conf = self._read_systemd_system_conf()
 		if (conf is not None):
-			fname = self._get_storage_filename()
-			cpu_affinity_saved = self._cmd.read_file(fname, err_ret = None, no_error = True)
-			self._cmd.unlink(fname)
-			if cpu_affinity_saved is None:
-				conf = self._del_key(conf, consts.SYSTEMD_CPUAFFINITY_VAR)
-			else:
-				conf = self._add_keyval(conf, consts.SYSTEMD_CPUAFFINITY_VAR, cpu_affinity_saved)
+			conf = self._del_key(conf, consts.SYSTEMD_CPUAFFINITY_VAR)
 			self._write_systemd_system_conf(conf)
 
 	def _instance_unapply_static(self, instance, rollback = consts.ROLLBACK_SOFT):
@@ -139,11 +132,6 @@ class SystemdPlugin(base.Plugin):
 		if verify:
 			return self._verify_value("cpu_affinity", v_unpacked, conf_affinity_unpacked, ignore_missing)
 		if enabling:
-			fname = self._get_storage_filename()
-			cpu_affinity_saved = self._cmd.read_file(fname, err_ret = None, no_error = True)
-			if conf_affinity is not None and cpu_affinity_saved is None and v_unpacked != conf_affinity_unpacked:
-				self._cmd.write_to_file(fname, conf_affinity, makedir = True)
-
 			log.info("setting '%s' to '%s' in the '%s'" % (consts.SYSTEMD_CPUAFFINITY_VAR, v_unpacked, consts.SYSTEMD_SYSTEM_CONF_FILE))
 			self._write_systemd_system_conf(self._add_keyval(conf, consts.SYSTEMD_CPUAFFINITY_VAR, v_unpacked))
 			return True
